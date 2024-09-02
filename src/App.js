5eb6e490d06d849e8b64a1dec529f45b4b6a600d
tonkla785 <156068067+tonkla785@users.noreply.github.com>
2024-09-02 11:29:14 +0700

    context_header_content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import {userContext} from './context'
+import Header2 from './context-header2'
+import Content2 from './context-content2'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+  let[user,setUser]=React.useState('')
+  return(
+    <userContext.Provider value={[user,setUser]}>
+      <Header2/>
+      <Content2/>
+    </userContext.Provider>
+  )
 }
-
-export default App;
